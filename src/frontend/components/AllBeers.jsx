eb288ff5564cf2f530edc57eb8fc55aa3fe3e1e4
Mallory Daniel <mllr.dnl@gmail.com>
2022-02-24 19:18:49 -0800

    switched AllBeers from axios to fetch request

--- src/frontend/components/AllBeers.jsx
+++ src/frontend/components/AllBeers.jsx
@@ -1,50 +1,59 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 
 export const AllBeers = () => {
 
-    const [beerListObj, setBeerListObj] = useState({});
     const [allBeerList, setAllBeerList] = useState([]);
+    const [loading, setLoading] = useState(false);
 
+    React.useEffect(() => {
     // var axios = require('axios');
 
-    function getAllBeerList() {
-        axios({
-            method: "GET",
-            url: "/all_beers",
-           
-        })
-        .then((response) => {
-            const res = response.data
+        fetch('/all_beers')
+            .then(res => res.json())
+            .then(data => setAllBeerList(data.data))
+            .catch(err => console.error(err))
+        
+    }, []);
+        // axios({
+        //     method: "GET",
+        //     url: "/all_beers",
+        
+        // })
+        // .then((response) => {
+        //     const res = response.data
 
-            const data = res.data    
+        //     const data = res.data    
             
-            setAllBeerList(data.data)
+        //     setAllBeerList(data.data)
 
 
-        }).catch((error) => {
-            if (error.response) {
-              console.log(error.response)
-              console.log(error.response.status)
-              console.log(error.response.headers)
-              }
-          })}
+        // }).catch((error) => {
+        //     if (error.response) {
+        //     console.log(error.response)
+        //     console.log(error.response.status)
+        //     console.log(error.response.headers)
+        //     }
+        // })}
+    console.log(Object.entries(allBeerList))
 
-
-
-        return(
+    if (loading) {
+        return <p>Data is loading...</p>
+    }
+    
+    return(
             <div className="beerlist">
                 <p>BEER LIST</p>
-                <button onClick={getAllBeerList}>Get it</button>
+                {/* <button onClick={getAllBeerList}>Get it</button> */}
                     <ul>
-                    {allBeerList.map((beer, index) => {
-                        return(
+                    {/* {allBeerList.map(({id, name }) => {
+                        
                             <div>
                                 <h3>BEER</h3>
-                                <li key={index}>{beer.id}, {beer.name}</li>
+                                <li key={id}>{id}, {name}</li>
                             </div>
-                        )
-                    })}
+                        
+                    })} */}
                   
                     </ul>
             
