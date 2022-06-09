8212c66ac20713595901370016888f2ea8d236c2
Mallory Daniel <mllr.dnl@gmail.com>
2022-06-08 19:50:02 -0700

    created navbar

---
+++ src/frontend/Navbar.jsx
@@ -0,0 +1,12 @@
+import React, { useState, useEffect } from "react";
+import './styles/navbar';
+
+export const Navbar = () => {
+
+    return(
+        <div className="navbar">
+
+        </div>
+    )
+
+}
